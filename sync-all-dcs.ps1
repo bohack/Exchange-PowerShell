f328f2e51b120296b4a024a98df10f94d0bf8d20
bohack <jbohack@gmail.com>
2016-07-06 16:08:00 -0400

    Added import-module
    Added import-module

--- sync-all-dcs.ps1
+++ sync-all-dcs.ps1
@@ -1,6 +1,7 @@
 # Bohack
 # 05/17/16
 # Force sync on Active Directory (Requires AD Module)
+Import-Module ActiveDirectory
 
 foreach ($dc in Get-ADGroupMember 'Domain Controllers') {
 	repadmin /syncall $dc.name /APed
